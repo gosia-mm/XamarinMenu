32c186febe75c0dfed6d64a0f2d5def944e16b9c
gosia-mm <malgosia.mrula@gmail.com>
2019-06-09 10:04:27 +0200

    Add RestService, FoodItemPage and native controls

---
+++ XamarinMenu/XamarinMenu/Services/MenuRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace XamarinMenu.Services
+{
+    public class MenuRestService
+    {
+        public HttpClient Client { get; set; }
+
+        public MenuRestService()
+        {
+            Client = new HttpClient(); // klient REST
+        }
+    }
+}
